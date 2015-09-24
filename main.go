f1101fe96b41dd69a8e241e5882a7db5bf5a0bec
Mike de Heij <mike@deheij.email>
2015-09-24 20:02:30 +0200

    Voor het lab

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	d := true
+	kanker := "NEGERS! "
+	fmt.Println(kanker, d)
+}
---
+++ test.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func kek() {
+
+	fmt.Println("hello world")
+	fmt.Print("kanker:")
+	jemoeder := 22
+	jeoma := "een kleuter"
+	fmt.Print("Nick Jetten: ", jemoeder, jeoma, jeoma, jemoeder)
+	fmt.Println()
+	fmt.Print(jeoma)
+}
