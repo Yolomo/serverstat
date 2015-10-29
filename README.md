f66c75af2b0838caf2742203fe3fc36754466f47
Mike de Heij <mike@deheij.email>
2015-10-29 22:06:09 +0100

    Added some functionality things

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ In short: getting and viewing statistics and current load of a server has never 
 
 Serverstat is a tool to automatically send server data to an API and process these values.
 
+![CI build](https://travis-ci.org/mdeheij/serverstat.svg?branch=master)
+
+
 ![Serverstat sample json](https://raw.githubusercontent.com/mdeheij/serverstat/gh-pages/screenshot-1.png)
+
--- server.go
+++ server.go
@@ -3,8 +3,16 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"github.com/bitly/go-simplejson"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func base64Encode(str string) string {
 	return base64.StdEncoding.EncodeToString([]byte(str))
@@ -18,88 +26,112 @@ func base64Decode(str string) string {
 	return string(data)
 }
 
+func getJsonVar(js *simplejson.Json, str string) string {
+	result, err := js.Get("hostname").String()
+	check(err)
+	return result
+}
+
+func v1ViewByHostname(c *gin.Context) {
+	hostname := c.Param("hostname")
+	dat, err := ioutil.ReadFile("hosts/" + hostname + ".json")
+	check(err)
+
+	fmt.Print(string(dat))
+
+	js, err := simplejson.NewJson(dat)
+	check(err)
+
+	labVar := getJsonVar(js, "hostname")
+
+	c.String(200, "<h1>Lab</h1><pre>"+labVar+"</pre>")
+
+}
 func v1ServerStatUpdate(c *gin.Context) {
 
 	hostname := base64Decode(c.PostForm("hostname"))
 	uptime := base64Decode(c.PostForm("uptime"))
 	sessions := base64Decode(c.PostForm("sessions"))
 	processes := base64Decode(c.PostForm("processes"))
-	processes_array := base64Decode(c.PostForm("processes_array"))
-	file_handles := base64Decode(c.PostForm("file_handles"))
-	file_handles_limit := base64Decode(c.PostForm("file_handles_limit"))
-	os_kernel := base64Decode(c.PostForm("os_kernel"))
-	os_name := base64Decode(c.PostForm("os_name"))
-	os_arch := base64Decode(c.PostForm("os_arch"))
-	cpu_name := base64Decode(c.PostForm("cpu_name"))
-	cpu_cores := base64Decode(c.PostForm("cpu_cores"))
-	cpu_freq := base64Decode(c.PostForm("cpu_freq"))
-	ram_total := base64Decode(c.PostForm("ram_total"))
-	ram_usage := base64Decode(c.PostForm("ram_usage"))
-	swap_total := base64Decode(c.PostForm("swap_total"))
-	swap_usage := base64Decode(c.PostForm("swap_usage"))
-	disk_array := base64Decode(c.PostForm("disk_array"))
-	disk_total := base64Decode(c.PostForm("disk_total"))
-	disk_usage := base64Decode(c.PostForm("disk_usage"))
+	processesarray := base64Decode(c.PostForm("processesarray"))
+	filehandles := base64Decode(c.PostForm("filehandles"))
+	filehandleslimit := base64Decode(c.PostForm("filehandleslimit"))
+	oskernel := base64Decode(c.PostForm("oskernel"))
+	osname := base64Decode(c.PostForm("osname"))
+	osarch := base64Decode(c.PostForm("osarch"))
+	cpuname := base64Decode(c.PostForm("cpuname"))
+	cpucores := base64Decode(c.PostForm("cpucores"))
+	cpufreq := base64Decode(c.PostForm("cpufreq"))
+	ramtotal := base64Decode(c.PostForm("ramtotal"))
+	ramusage := base64Decode(c.PostForm("ramusage"))
+	swaptotal := base64Decode(c.PostForm("swaptotal"))
+	swapusage := base64Decode(c.PostForm("swapusage"))
+	diskarray := base64Decode(c.PostForm("diskarray"))
+	disktotal := base64Decode(c.PostForm("disktotal"))
+	diskusage := base64Decode(c.PostForm("diskusage"))
 	connections := base64Decode(c.PostForm("connections"))
 	nic := base64Decode(c.PostForm("nic"))
 	ipv4 := base64Decode(c.PostForm("ipv4"))
 	ipv6 := base64Decode(c.PostForm("ipv6"))
 	rx := base64Decode(c.PostForm("rx"))
 	tx := base64Decode(c.PostForm("tx"))
-	rx_gap := base64Decode(c.PostForm("rx_gap"))
-	tx_gap := base64Decode(c.PostForm("tx_gap"))
+	rxgap := base64Decode(c.PostForm("rxgap"))
+	txgap := base64Decode(c.PostForm("txgap"))
 	load := base64Decode(c.PostForm("load"))
-	load_cpu := base64Decode(c.PostForm("load_cpu"))
-	load_io := base64Decode(c.PostForm("load_io"))
-	ping_1 := base64Decode(c.PostForm("ping_1"))
+	loadcpu := base64Decode(c.PostForm("loadcpu"))
+	loadio := base64Decode(c.PostForm("loadio"))
+	ping1 := base64Decode(c.PostForm("ping1"))
 
 	c.JSON(200, gin.H{
-		"status":             "received",
-		"hostname":           hostname,
-		"uptime":             uptime,
-		"sessions":           sessions,
-		"processes":          processes,
-		"processes_array":    processes_array,
-		"file_handles":       file_handles,
-		"file_handles_limit": file_handles_limit,
-		"os_kernel":          os_kernel,
-		"os_name":            os_name,
-		"os_arch":            os_arch,
-		"cpu_name":           cpu_name,
-		"cpu_cores":          cpu_cores,
-		"cpu_freq":           cpu_freq,
-		"ram_total":          ram_total,
-		"ram_usage":          ram_usage,
-		"swap_total":         swap_total,
-		"swap_usage":         swap_usage,
-		"disk_array":         disk_array,
-		"disk_total":         disk_total,
-		"disk_usage":         disk_usage,
-		"connections":        connections,
-		"nic":                nic,
-		"ipv4":               ipv4,
-		"ipv6":               ipv6,
-		"rx":                 rx,
-		"tx":                 tx,
-		"rx_gap":             rx_gap,
-		"tx_gap":             tx_gap,
-		"load":               load,
-		"load_cpu":           load_cpu,
-		"load_io":            load_io,
-		"ping_1":             ping_1,
+		"status":           "received",
+		"hostname":         hostname,
+		"uptime":           uptime,
+		"sessions":         sessions,
+		"processes":        processes,
+		"processesarray":   processesarray,
+		"filehandles":      filehandles,
+		"filehandleslimit": filehandleslimit,
+		"oskernel":         oskernel,
+		"osname":           osname,
+		"osarch":           osarch,
+		"cpuname":          cpuname,
+		"cpucores":         cpucores,
+		"cpufreq":          cpufreq,
+		"ramtotal":         ramtotal,
+		"ramusage":         ramusage,
+		"swaptotal":        swaptotal,
+		"swapusage":        swapusage,
+		"diskarray":        diskarray,
+		"disktotal":        disktotal,
+		"diskusage":        diskusage,
+		"connections":      connections,
+		"nic":              nic,
+		"ipv4":             ipv4,
+		"ipv6":             ipv6,
+		"rx":               rx,
+		"tx":               tx,
+		"rxgap":            rxgap,
+		"txgap":            txgap,
+		"load":             load,
+		"loadcpu":          loadcpu,
+		"loadio":           loadio,
+		"ping1":            ping1,
 	})
 
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+	r.GET("/lab", func(c *gin.Context) {
+
+		labVar := "Test."
+		c.String(200, "<h1>Lab</h1><pre>"+labVar+"</pre>")
 	})
 
 	v1 := r.Group("/v1")
 	{
 		v1.POST("/serverstat/update", v1ServerStatUpdate)
+		v1.GET("/serverstat/view/:hostname/", v1ViewByHostname)
 		v1.GET("/serverstat/ping", func(c *gin.Context) {
 			c.String(200, "Pong! ")
 		})
